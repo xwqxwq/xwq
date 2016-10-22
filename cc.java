9b9ffae5d7de29bc9c39c16b0230310cb887bff2
1475367249 <1475367249@qq.com>
2016-10-23 00:35:52 +0800

    xwq

---
+++ cc.java
@@ -0,0 +1,14 @@
+package bb;
+
+public class cc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aa");
+
+	}
+
+}
